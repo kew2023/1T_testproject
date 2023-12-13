61cc9916e223edf6856d61a74794c6fb3a97c123
kew2023 <igorlov2604@gmail.com>
2023-12-13 23:48:50 +0300

    day1

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+    <Header v-model:userInfo="userInfo">
+    </Header>
+
+    <main>
+        <Profile></Profile>
+        <div class="main__container">
+            <div class="main__line"></div>
+            <div class="main__title">Отзывы</div>
+            <div class="main__author"></div>
+            <div class="main__reviews">
+                <div class="main__reviews"></div>
+            </div>
+        </div>
+    </main>
+    <!--<nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view />-->
+</template>
+
+<style>
+* {
+    padding: 0;
+}
+
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    padding-left: 130px;
+    padding-top: 130px;
+    padding-right: 130px;
+}
+
+main {
+    display: flex;
+}
+</style>
+
+<script>
+import Header from "@/components/Header.vue";
+import Profile from "@/components/Profile.vue";
+export default {
+    data () {
+        return {
+            userInfo: {
+                rating: 0,
+                reviews: 0,
+                sales: 0,
+                buy: 0,
+            }
+        };
+    },
+    components: { Header, Profile }
+};
+</script>
---
+++ src/components/Profile.vue
@@ -0,0 +1,296 @@
+<template lang="">
+    <div class="profile">
+            <div class="profile__header">
+                <img class="profile__icon" src="../assets/icon.png" alt="Икон">
+                <div class="profile__info">
+                    <p class="profile__name">Ivan_Mazepa</p>
+                    <p class="profile__status online">Онлайн</p>
+                    <p class="profile__status offline" style="display: none;">Офлайн</p>
+                </div>
+            </div>
+            <p class="profile__description">Продажа голды WOW 24/7</p>
+            <div class="profile__register">
+                <p class="register__info">Регистрация: <span>Месяц назад</span> </p>
+                <p class="register__date">25 ноября 2022, в 11:33</p>
+            </div>
+            <nav>
+                <div class="nav__item" id="nav1">Сделки</div>
+                <div class="nav__item" id="nav2">Объявления</div>
+                <div class="nav__item active" id="nav3">Отзывы</div>
+                <div class="nav__item" id="nav4">Редактировать профиль</div>
+                <div class="nav__item exit" id="nav5"> Выйти</div>
+            </nav>
+        </div>
+</template>
+<script>
+export default {
+    name: "Profile",
+};
+</script>
+<style scoped>
+.profile {
+    width: 320px;
+    padding: 25px;
+    border-radius: 20px;
+    border: 1px solid rgba(255, 255, 255, 0.07);
+    background: rgba(255, 255, 255, 0.03);
+    box-shadow: 2px 0px 10px 0px rgba(6, 3, 9, 0.05), 30px 25px 48px 8px rgba(6, 3, 9, 0.10);
+    backdrop-filter: blur(5px);
+}
+
+.profile__header {
+    display: flex;
+    align-items: center;
+}
+
+.profile__icon {
+    width: 34.426px;
+    height: 34.426px;
+    padding: 17.79px;
+    border-radius: 20px;
+    border: 1px solid rgba(255, 255, 255, 0.05);
+    background: rgba(255, 255, 255, 0.03);
+    box-shadow: 2px 0px 10px 0px rgba(6, 3, 9, 0.05), 30px 25px 48px 8px rgba(6, 3, 9, 0.10);
+    backdrop-filter: blur(5px);
+}
+
+.profile__info {
+    margin-left: 15px;
+}
+
+.profile__name {
+    color: #FFFFFF;
+    font-family: Montserrat;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+}
+
+.profile__status {
+    font-family: Montserrat;
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+}
+
+.online,
+.online::before {
+    color: #29C226
+}
+
+.online::before {
+    content: '';
+    display: inline-block;
+    margin-bottom: 2px;
+    width: 5px;
+    height: 5px;
+    -moz-border-radius: 2.5px;
+    -webkit-border-radius: 2.5px;
+    border-radius: 2.5px;
+    color: #29C226;
+    background-color: #29C226;
+    margin-right: 10px;
+    box-shadow: 0px 0px 5px 4px rgba(41, 194, 38, 0.20);
+}
+
+
+.offline {
+    color: #b42d2d;
+}
+
+.offline::before {
+    margin-bottom: 2px;
+    content: '';
+    display: inline-block;
+    width: 5px;
+    height: 5px;
+    -moz-border-radius: 2.5px;
+    -webkit-border-radius: 2.5px;
+    border-radius: 2.5px;
+    color: #b42d2d;
+    background-color: #b42d2d;
+    margin-right: 10px;
+    box-shadow: 0px 0px 5px 4px rgba(41, 194, 38, 0.20);
+}
+
+.profile__description {
+    color: #7D7781;
+    font-family: Montserrat;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+}
+
+.profile__register {
+    width: 95%;
+    border-radius: 15px;
+    background: rgba(14, 9, 19, 0.50);
+    padding-left: 15px;
+    padding-top: 15px;
+    padding-bottom: 15px;
+}
+
+.register__info {
+    display: block;
+    color: #7D7781;
+    font-family: Montserrat;
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 26px;
+}
+
+.register__info span {
+    display: inline;
+}
+
+.register__date {
+    display: block;
+    color: #FFF;
+    font-family: Montserrat;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 26px;
+}
+
+nav {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+}
+
+.nav__item {
+    display: flex;
+    align-items: center;
+    width: 270px;
+    height: 60px;
+    margin-bottom: 0px;
+    padding-left: 15px;
+
+    color: #7D7781;
+    font-family: Montserrat;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+}
+
+.nav__item::before {
+    margin-right: 15px;
+}
+
+nav>.active {
+    color: #FFFFFF;
+    border-radius: 15px;
+    border: 1px solid rgba(255, 255, 255, 0.07);
+    background: rgba(255, 255, 255, 0.03);
+    box-shadow: 2px 0px 10px 0px rgba(6, 3, 9, 0.05), 30px 25px 48px 8px rgba(6, 3, 9, 0.10);
+    backdrop-filter: blur(5px);
+}
+
+#nav1::before {
+    content: url("../assets/icon_nav_1.svg");
+}
+
+#nav2::before {
+    content: url("../assets/icon_nav_2.svg");
+}
+
+#nav3::before {
+    content: url("../assets/icon_nav_3.svg");
+}
+
+#nav4::before {
+    content: url("../assets/icon_nav_4.svg");
+}
+
+#nav5::before {
+    content: url("../assets/icon_nav_5.svg");
+}
+
+.exit {
+    color: #FA3939;
+}
+
+@media (max-width: 1919px) {
+
+    .profile {
+        width: 234px;
+        padding: 20px;
+    }
+
+    .profile__icon {
+        width: 24.59px;
+        height: 24.59px;
+        padding: 12.71px;
+        border-radius: 15px;
+    }
+
+    .profile__info {
+        margin-left: 15px;
+        padding-top: 4px;
+        padding-bottom: 4px;
+    }
+
+    .profile__name {
+        font-size: 14px;
+    }
+
+    .profile__status {
+        font-size: 12px;
+    }
+
+
+    .profile__description {
+        color: #7D7781;
+        font-family: Montserrat;
+        font-size: 12px;
+        font-style: normal;
+        font-weight: 500;
+        line-height: normal;
+    }
+
+    .profile__register {
+        border-radius: 15px;
+        background: rgba(14, 9, 19, 0.50);
+        padding-left: 15px;
+        padding-top: 15px;
+        padding-bottom: 15px;
+    }
+
+    .register__info {
+        font-size: 12px;
+        line-height: 20px;
+    }
+
+    .register__info span {
+        display: inline;
+    }
+
+    .register__date {
+        display: block;
+        color: #FFF;
+        font-family: Montserrat;
+        font-size: 14px;
+        font-style: normal;
+        font-weight: 500;
+        line-height: 26px;
+    }
+
+    .nav__item {
+        width: 220px;
+        height: 48px;
+        padding-left: 15px;
+        font-size: 14px;
+    }
+}
+
+@media (max-width: 1279px) {}
+
+@media (max-width: 1023px) {}
+</style>
